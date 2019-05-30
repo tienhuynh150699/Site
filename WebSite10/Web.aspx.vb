2b62ae6618c857cf7da03fcdcdab93860b7c1e18
tienhuynh150699@gmail.com <tienhuynh150699@gmail.com>
2019-05-30 10:31:23 +0700

    1

--- WebSite10/Web.aspx.vb
+++ WebSite10/Web.aspx.vb
@@ -3,7 +3,7 @@ Partial Class Web
     Inherits BasePage
 
     Protected Sub sutmitbutton_Click(sender As Object, e As EventArgs) Handles sutmitbutton.Click
-        Result.Text = "Cảm ơn bạn đã đăng ký và tên người đăng ký là " & ĐăngKý.Text
+        Result.Text = "Cảm ơn bạn đã sử dụng dịch vụ Grap " & ĐăngKý.Text
     End Sub
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
 
